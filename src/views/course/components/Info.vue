58dcaa0048a4aaa394629eb376d5fc58e1520f49
superwangmsi <245801806@qq.com>
2021-06-15 23:10:15 +0800

    1课程管理路由2课程保存三步

---
+++ src/views/course/components/Info.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app-container">
+    <!-- 课程信息表单 TODO -->
+
+    <div style="text-align:center">
+      <el-button :disabled="saveBtnDisabled" type="primary" @click="saveAndNext()">保存并下一步</el-button>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      saveBtnDisabled: false // 按钮禁用
+    }
+  },
+
+  methods: {
+    // 保存并且下一步
+    saveAndNext() {
+      this.saveBtnDisabled = true
+      this.$parent.active = 1
+    }
+  }
+}
+</script>
---
+++ src/views/course/form.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="app-container">
+
+    <h2 style="text-align: center;">发布新课程</h2>
+    <el-steps :active="active" finish-status="success" simple style="margin-bottom: 40px">
+      <el-step title="填写课程基本信息" />
+      <el-step title="创建课程大纲" />
+      <el-step title="发布课程" />
+    </el-steps>
+
+    <!-- step1: Info -->
+    <info v-if="active === 0" />
+
+    <!-- step2 : ChapterIndex-->
+    <chapter v-if="active === 1" />
+    <!-- step3 : Publish -->
+    <publish v-if="active === 2 || active === 3" />
+
+  </div>
+</template>
+
+<script>
+// 引入子组件（自定义组件）
+import Info from '@/views/course/components/Info'
+import Chapter from '@/views/course/components/Chapter' // index可以省略
+import Publish from '@/views/course/components/Publish'
+export default {
+  // 注册组件
+  components: {
+    Info, Chapter, Publish
+  },
+
+  data() {
+    return {
+      active: 0
+    }
+  }
+}
+</script>
