dcc47f40280fca74685068773225733bb94914aa
wang1660s <1660s@qq.com>
2021-06-20 23:03:15 +0800

    step2章节的页面显示

--- src/views/course/components/Chapter/Index.vue
+++ src/views/course/components/Chapter/Index.vue
@@ -1,8 +1,45 @@
 <template>
   <div class="app-container">
-    Chapter Index
+    <!-- 添加章节按钮 -->
+    <div>
+      <el-button type="primary" @click="addChapter()">添加章节</el-button>
+    </div>
 
-    <!-- 章节列表 TODO -->
+    <chapter-form ref="chapterForm" />
+
+    <!-- 章节列表 -->
+    <ul class="chapterList">
+      <li
+        v-for="chapter in chapterList"
+        :key="chapter.id">
+        <p>
+          {{ chapter.title }}
+          <span class="acts">
+            <el-button type="text">添加课时</el-button>
+            <el-button type="text">编辑</el-button>
+            <el-button type="text" @click="removeChapterById(chapter.id)">删除</el-button>
+          </span>
+        </p>
+        <!-- 视频 -->
+        <ul class="chapterList videoList">
+          <li
+            v-for="video in chapter.children"
+            :key="video.id">
+            <p>
+              {{ video.title }}
+              <el-tag v-if="!video.videoSourceId" size="mini" type="danger">
+                {{ '尚未上传视频' }}
+              </el-tag>
+              <span class="acts">
+                <el-tag v-if="video.free" size="mini" type="success">{{ '免费观看' }}</el-tag>
+                <el-button type="text">编辑</el-button>
+                <el-button type="text">删除</el-button>
+              </span>
+            </p>
+          </li>
+        </ul>
+      </li>
+    </ul>
 
     <div style="text-align:center">
       <el-button type="primary" @click="prev()">上一步</el-button>
@@ -13,9 +50,74 @@
 </template>
 
 <script>
+import chapterApi from '@/api/chapter'
+
+import ChapterForm from '@/views/course/components/Chapter/Form' // 引入自定义组件
+
 export default {
 
+  components: { ChapterForm }, // 引入自定义组件
+
+  data() {
+    return {
+      chapterList: []
+    }
+  },
+
+  created() {
+    this.fetchNodeList()
+  },
+
   methods: {
+
+    // 获取远程数据
+    fetchNodeList() {
+      chapterApi.getNestedTreeList(this.$parent.courseId).then(response => {
+        this.chapterList = response.data.datalist
+      })
+    },
+
+    // 删除章节
+    removeChapterById(chapterId) {
+      this.$confirm('此操作将永久删除该章节，是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        return chapterApi.removeById(chapterId)
+      }).then(response => {
+        this.fetchNodeList()
+        this.$message.success(response.message)
+      }).catch((response) => {
+        if (response === 'cancel') {
+          this.$message.info('取消删除')
+        }
+      })
+    },
+
+    // 保存章节
+    save() {
+      this.chapter.courseId = this.$parent.$parent.courseId
+      chapterApi.save(this.chapter).then(response => {
+        this.$message.success(response.message)
+        // 关闭组件
+        this.close()
+        // 刷新列表
+        this.$parent.fetchNodeList()
+      })
+    },
+
+    // 添加章节
+    addChapter() {
+      this.$refs.chapterForm.open()
+    },
+
+    close() {
+      this.dialogVisible = false
+      // 重置表单
+      this.resetForm()
+    },
+
     // 上一步
     prev() {
       this.$parent.active = 0
@@ -29,3 +131,43 @@ export default {
 
 }
 </script>
+
+<style>
+.chapterList{
+    position: relative;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+}
+.chapterList li{
+  position: relative;
+}
+.chapterList p{
+  float: left;
+  font-size: 20px;
+  margin: 10px 0;
+  padding: 10px;
+  height: 70px;
+  line-height: 50px;
+  width: 100%;
+  border: 1px solid #DDD;
+}
+.chapterList .acts {
+    float: right;
+    font-size: 14px;
+}
+
+.videoList{
+  padding-left: 50px;
+}
+.videoList p{
+  float: left;
+  font-size: 14px;
+  margin: 10px 0;
+  padding: 10px;
+  height: 50px;
+  line-height: 30px;
+  width: 100%;
+  border: 1px dashed #DDD;
+}
+</style>
